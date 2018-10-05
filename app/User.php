cd400714fae63ae0874abb3ebc291ed1688dab30
John Carlo <totodile14@yahoo.com>
2018-10-05 22:39:55 +0800

    Initial commit

---
+++ app/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name', 'middle_name', 'last_name', 'email', 'password', 'referral_code'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+    public function individual(){
+        return $this->hasOne('App\Individual');
+    }
+
+    public function entity(){
+        return $this->hasMany('App\Entity');
+    }
+}
---
+++ database/migrations/2018_10_05_124736_create_offers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('bank_id');
+            $table->integer('loan_id');
+            $table->string('specification');
+            $table->integer('term');
+            $table->float('interest_rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
